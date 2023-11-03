006e235528bedce335f3eb9d862d38305f609f4f
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-03 19:09:14 +0000

    Fri, Nov 3, 2023, 7:09 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -13,4 +13,25 @@ AND bakery_security_logs.day = 28
 AND bakery_security_logs.minute >= 15
 AND bakery_security_logs.month <= 25;
 
+SELECT people.name, atm_transactions.transaction_type FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+WHERE atm_transactions.year = 2021
+AND atm_transactions.month = 7
+AND atm_transactions.day = 28
+AND atm_location = "Leggett Street"
+AND atm_transactions.transaction_type = "withdraw";
+
+SELECT caller, caller_name, receiver, receiver_name FROM phone_calls
+WHERE year = 2021
+AND month = 7
+AND day = 28
+AND duration < 60;
+
+ALTER TABLE phone_calls
+ADD caller_name text;
+
+ALTER TABLE phone_calls
+ADD receiver_name text;
+
 
