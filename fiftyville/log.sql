52f65895780bd466f81df58b12d8002b4e52c79d
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-03 19:40:11 +0000

    Fri, Nov 3, 2023, 7:40 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -45,17 +45,6 @@ AND day = 28
 AND duration < 60;
 
 
-UPDATE flights
-SET origin_airport_id = airports.city
-FROM airports
-WHERE flights.origin_airport_id = airports.id;
-
-UPDATE flights
-SET destination_airport_id = airports.city
-FROM airports
-WHERE flights.destination_airport_id = airports.id;
-
-
 SELECT id, hour, minute, origin_airport_id, destination_airport_id FROM flights
 WHERE year = 2021
 AND month = 7
