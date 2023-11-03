991c3c8dac4bfbac40b63fd93b12d51824b9b9a3
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-03 18:59:15 +0000

    Fri, Nov 3, 2023, 6:59 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,4 +5,12 @@ WHERE year = 2021 AND month = 7 AND day=28;
 SELECT transcript FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 
-S
+SELECT bakery_security_logs.activity, bakery_security_logs.licence_plate, people.name FROM people
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+WHERE bakery_security_logs.year = 2021
+AND bakery_security_logs.month = 7
+AND bakery_security_logs.day = 28
+AND bakery_security_logs.minute >= 15
+AND bakery_security_logs.month <= 25;
+
+
